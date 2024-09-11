877c36c8d5115d0ba0db4d91114e704624bf6954
SebassRM128 <a.22308051281303@cbtis128.edu.mx>
2024-09-11 07:38:16 -0600

    Add files via upload

---
+++ mi_conunto.py
@@ -0,0 +1,7 @@
+print("Eejemplo conjunto")
+tipodeportes={"futbol","tenis","basquetol"}
+print(tipodeportes)
+print("Lista elementos del conjunto tipodeportes loop for")
+print(tipodeportes)
+for rojas in tipodeportes:
+    print(rojas)
